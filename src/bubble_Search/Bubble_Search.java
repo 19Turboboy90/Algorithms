3b7f105b8472f8265f512faaaca2a44264b5d3c2
Илья <90090224+19Turboboy90@users.noreply.github.com>
2021-12-28 22:01:00 +0300

    adding Bubble_Search

---
+++ src/bubble_Search/Bubble_Search.java
@@ -0,0 +1,41 @@
+package bubble_Search;
+
+public class Bubble_Search {
+    public static void main(String[] args) {
+        int[] array = new int[]{34, 3, 56, 27, 85, 25, 83, 66, 28, 57, 48};
+        // sorting1(array);
+        sorting2(array);
+
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+
+    }
+
+    private static void sorting1(int[] array) {
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    int tmp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = tmp;
+                    isSorted = false;
+                }
+            }
+        }
+    }
+
+    private static void sorting2(int[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
+    }
+}
